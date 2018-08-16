f414daa45d89bb72c69cdefbc788cf458d9d47bc
Elisha Riedlinger <elisha@novicemail.com>
2018-08-15 22:22:40 -0700

    Add wchar_t support to logging

--- Common/Logging.h
+++ Common/Logging.h
@@ -28,6 +28,12 @@ private:
 	static std::ofstream LOG;
 };
 
+static std::ostream& operator<<(std::ostream& os, const wchar_t* wchr)
+{
+	std::wstring ws(wchr);
+	return os << std::string(ws.begin(), ws.end()).c_str();
+}
+
 static void logf(char * fmt, ...)
 {
 	va_list ap;
@@ -35,6 +41,18 @@ static void logf(char * fmt, ...)
 	auto size = vsnprintf(nullptr, 0, fmt, ap);
 	std::string output(size + 1, '\0');
 	vsprintf_s(&output[0], size + 1, fmt, ap);
+	Log() << output.c_str();
+	va_end(ap);
+}
+
+static void logf(wchar_t * fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+#pragma warning(suppress: 4996)
+	auto size = _vsnwprintf(nullptr, 0, fmt, ap);
+	std::wstring output(size + 1, '\0');
+	vswprintf_s(&output[0], size + 1, fmt, ap);
 	Log() << output.c_str();
 	va_end(ap);
 }
